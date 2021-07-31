63f13f964176706215f2bb95fd127fb13492b4f4
Gariell <52863409+Gariell@users.noreply.github.com>
2021-08-01 01:04:13 +0300

    improv code readability and minor fixes

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const musicContainer = document.querySelector('.music-container');
 const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
-
 const audio = document.querySelector('#audio');
 const progress = document.querySelector('.progress');
 const progressTimes = document.querySelector('#progressTime');
 const progressTimesCounter = document.querySelector('#progressTimeCounter');
 const progressContainer = document.querySelector('.progress-container');
-
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
-
 const volumePlus = document.querySelector('#volumePlus');
 const volumeMinus = document.querySelector('#volumeMinus');
-
 const volumeRange = document.getElementById("volumeRange");
 const volumeRangeOutput = volumeRange / 100
 
@@ -35,34 +31,37 @@ function loadSong(song) {
   audioTimeCounter()
 }
 
+// функция счета времени песни
 function audioTimeCounter() {
-
-  function str_pad_left(string,pad,length) {
-    return (new Array(length+1).join(pad)+string).slice(-length);
-  }
-  
+  function str_pad_left(string,pad,length) { return (new Array(length+1).join(pad)+string).slice(-length);}
   setInterval(() => {
-
-
     let audioDuration = audio.duration
     let timeMinutes = Math.floor(audioDuration / 60);
     let timeSeconds = audioDuration - timeMinutes * 60;
-    progressTimes.innerText = timeMinutes + ":" + Math.floor(timeSeconds)
     let timeMinutesCounter = Math.floor(audio.currentTime / 60);
     let timeSecondsCounter = audio.currentTime - timeMinutesCounter * 60;
-    progressTimesCounter.innerHTML = timeMinutesCounter + ":" + str_pad_left(Math.floor(timeSecondsCounter),'0',2)
-
-  }, 100);
+    audioTime = timeMinutes + ":" + Math.floor(timeSeconds) + " / " + timeMinutesCounter + ":" + str_pad_left(Math.floor(timeSecondsCounter),'0',2)
+    // if (audioTime == NaN) {
+    //   progressTimes.innerText = "0:00 / 0:00"
+    // }else { 
+      progressTimes.innerText = audioTime 
+    // }
+    console.log(audioTime )
+  }, 1000);
 }
 
+// функция начала воспроизведения песни
 function playSong() {
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play')
   playBtn.querySelector('i.fas').classList.add('fa-pause')
   audio.play()
-  audioTimeCounter()
+  audio.onloadedmetadata = function() {
+    audioTimeCounter()
+  }
 }
 
+// функция приостановки воспроизведения песни
 function pauseSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play')
@@ -70,28 +69,26 @@ function pauseSong() {
   audio.pause()
 }
 
+// функция обновления полоски прогресса песни
 function updateProgress(e) {
   const {duration, currentTime} = e.srcElement
   const progressPercent = (currentTime / duration) * 100
   progress.style.width = `${progressPercent}%`
-  
 }
 
+// функция перемотки песни при клике на полосу прогресса
 function setProgress (e) {
   const width = this.clientWidth
   const clickX = e.offsetX
   const duration = audio.duration
   audio.currentTime = (clickX / width) * duration
-  
 }
 
 //регулировка звука
 let btnClick = false
 let intervalVolumeListener
 const volumeDeviation = 0.05
-
 function volumeplus() {
-
   if(btnClick == true) {
     audio.volume = Math.min(1, audio.volume + volumeDeviation)
     volumeRange.value = audio.volume * 100
@@ -104,16 +101,19 @@ function volumeminus() {
   }
 }
 
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play')
+  if(isPlaying) {pauseSong()} else {playSong()}
+})
 volumePlus.addEventListener('mousedown', () => {
   btnClick = true
   intervalVolumeListener = setInterval(volumeplus, 50)
 }, false)
-
 volumePlus.addEventListener('mouseup', () => {
   btnClick = false
   clearInterval(intervalVolumeListener)
 }, false)
-
 volumeMinus.addEventListener('mousedown', () => {
   btnClick = true
   intervalVolumeListener = setInterval(volumeminus, 50)
@@ -123,22 +123,9 @@ volumeMinus.addEventListener('mouseup', () => {
   btnClick = false
   clearInterval(intervalVolumeListener)
 }, false)
-
 volumeRange.oninput = function() {
   audio.volume = volumeRange.value / 100
 }
-
-
-
-// Event listeners
-playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play')
-  if(isPlaying) {
-    pauseSong()
-  } else {
-    playSong()
-  }
-})
 
 // функции переключения песен
 function prevSong() {
@@ -148,6 +135,7 @@ function prevSong() {
   }
   loadSong(songs[songIndex])
   playSong()
+  
 }
 function nextSong() {
   songIndex++
@@ -165,3 +153,5 @@ audio.addEventListener('timeupdate', updateProgress)
 progressContainer.addEventListener('click', setProgress)
 audio.addEventListener('ended', nextSong)
 audioTimeCounter()
+
+
